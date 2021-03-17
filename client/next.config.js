d2ec25cebbb2446d6cc677312609555cdbe81093
shinnguyen0105 <nhatncgcd17202@fpt.edu.vn>
2021-03-17 22:51:14 +0700

    next js config pathmap and fix ui register page

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,27 @@
-const withPWA = require('next-pwa');
+//const withPWA = require('next-pwa');
 require('dotenv').config();
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-  },
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//   },
+//   exportPathMap: async function () {
+//     return {
+//       '/': { page: '/' },
+//       '/dashboard': { page: '/dashboard' },
+//       '/create': { page: '/create' },
+//       '/login': { page: '/login' },
+//       '/register': { page: '/register' },
+//       '/profile': { page: '/profile' },
+//       '/create-channel': { page: '/create-channel' },
+//       '/404': { page: '/404' },
+//     };
+//   },
+//   env: {
+//     API_URL: process.env.API_URL,
+//   },
+// });
+module.exports = {
   exportPathMap: async function () {
     return {
       '/': { page: '/' },
@@ -20,4 +37,4 @@ module.exports = withPWA({
   env: {
     API_URL: process.env.API_URL,
   },
-});
+};
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -19,7 +19,7 @@ import {
 
 import Image from 'next/image';
 
-const Login = () => {
+const Register = () => {
   return (
     <main>
       <section className='section section-shaped section-lg'>
@@ -37,51 +37,12 @@ const Login = () => {
           <Row className='justify-content-center'>
             <Col lg='5'>
               <Card className='bg-secondary shadow border-0'>
-                <CardHeader className='bg-white pb-5'>
-                  <div className='text-muted text-center mb-3'>
-                    <small>Sign in with</small>
-                  </div>
-                  <div className='btn-wrapper text-center'>
-                    <Button
-                      className='btn-neutral btn-icon'
-                      color='default'
-                      href='#pablo'
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <span className='btn-inner--icon mr-1 pb-2'>
-                        <Image
-                          alt='...'
-                          className='mt-1'
-                          width='20'
-                          height='20'
-                          src='/github.svg'
-                        />
-                      </span>
-                      <span className='btn-inner--text'>Github</span>
-                    </Button>
-                    <Button
-                      className='btn-neutral btn-icon ml-1'
-                      color='default'
-                      href='#pablo'
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <span className='btn-inner--icon mr-1 pb-2'>
-                        <Image
-                          alt='...'
-                          className='mt-1'
-                          width='20'
-                          height='20'
-                          src='/google.svg'
-                        />
-                      </span>
-                      <span className='btn-inner--text'>Google</span>
-                    </Button>
+                <CardHeader className='bg-white'>
+                  <div className='text-center text-muted'>
+                    <h4>Or sign up with credentials</h4>
                   </div>
                 </CardHeader>
                 <CardBody className='px-lg-5 py-lg-5'>
-                  <div className='text-center text-muted mb-4'>
-                    <small>Or sign up with credentials</small>
-                  </div>
                   <Form role='form'>
                     <FormGroup>
                       <InputGroup className='input-group-alternative mb-3'>
@@ -166,4 +127,4 @@ const Login = () => {
   );
 };
 
-export default withRouter(Login);
+export default withRouter(Register);
